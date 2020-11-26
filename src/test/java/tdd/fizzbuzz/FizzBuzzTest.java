9e0ba5ef2c71c0cb7e46402f04e7ce1a74b57b67
Ernest Cheung <ernest.cheung@oocl.com>
2020-11-26 13:56:06 +0800

    [FizzBuzz][Ernest] TestFail: should_return_1_when_count_off_given_order_number_1

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,22 @@
 package tdd.fizzbuzz;
 
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
+    @Test
+    public void should_return_1_when_count_off_given_order_number_1() {
+        //given
+        int order = 1;
+        String expected = "1";
+
+        //when
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        //then
+        assertEquals(expected, fizzBuzz.countOff(order));
+    }
+
 }
